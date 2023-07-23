9c46994d9b3b71c5e58b23bfea90126f759cbd50
Enrico Giurin <enricogiurin@gmail.com>
2023-07-23 23:22:13 +0200

    structure refactoring

--- src/main/java/org/wealthtracker/backend/WealthTackerRestApiApplication.java
+++ src/main/java/org/wealthtracker/backend/WealthTackerRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WealthtrackerRestApiApplication {
+public class WealthTackerRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WealthtrackerRestApiApplication.class, args);
+		SpringApplication.run(WealthTackerRestApiApplication.class, args);
 	}
 
 }
